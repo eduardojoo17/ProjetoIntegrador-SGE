be754d1f9b625f178c26c731328537ff4940afd2
Pedro Henrique Pais Santos <pet0160430@aluno.firjansenaisesi.com.br>
2026-06-01 19:19:23 -0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import type { Application } from "express";
+import { AppDataSource } from "./data-source.js";
+
+const app: Application = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+AppDataSource.initialize()
+  .then(() => {
+    console.log("Banco conectado!");
+    app.listen(process.env.PORT, () => {
+      console.log(`Servidor rodando em http://localhost:${port}`);
+    });
+  })
+  .catch((error) => console.log("Erro ao conectar no banco: ", error));
